f4d81be80bda656af57869039930714916e73791
Yuya7124 <gat.x105s02.ys@icloud.com>
2024-04-24 16:39:43 +0900

    OpenCV(Python)⑦

--- E-Learning (Python)/opencv/camera_py.py
+++ E-Learning (Python)/opencv/camera_py.py
@@ -4,45 +4,88 @@ import pyocr
 from PIL import Image
 import pyocr.builders
 import numpy as np
-from keras.api.datasets import mnist
+from keras.api.datasets.mnist import load_data
 from keras.api.models import Sequential, load_model
-from keras.src.layers.core import Dense, Activation
-from keras.src.layers import Conv2D, MaxPooling2D, Dropout, Reshape
-from keras.src.utils import np_utils
+from keras.src.layers import Dense, Activation ,Conv2D, Flatten, MaxPooling2D, Dropout, Reshape
+from keras.src.utils.numerical_utils import to_categorical
 
-def mnist():
-  height = 28 
-  width = 28
-  (x_train, y_train), (x_test, y_test) = mnist.load_data()
+height = 28 
+width = 28
+(x_train, y_train), (x_test, y_test) = load_data()
 
-  x_train = x_train.reshape(60000, height * width) / 255
-  x_test = x_test.reshape(10000, height * width) / 255
+x_train = np.array(x_train) / 255
+x_test = np.array(x_test) / 255
 
-  y_train = np_utils.to_categorical(y_train)
-  y_test = np_utils.to_categorical(y_test)
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 
-  model = Sequential()
-  model.add(Dense(512, input_dim=(height * width)))
+tools = pyocr.get_available_tools()
+tool = tools[0]
+
+h5_file = "h5/drawing_text_test.h5"
+cap_file = "image/capture/sample.png"
+
+# モデル構築
+model = Sequential()
+
+def mnist(model):
+  layers(model)
+  # cv2.namedWindow('src')
+  # cv2.namedWindow('dst')
+  
+  # コンパイル
+  model.compile(optimizer="sgd", loss="categorical_crossentropy", metrics=["accuracy"])
+  
+  learning(model)
+
+  # モデル保存
+  model.save(h5_file)
+  # モデル削除
+  del model
+
+def layers(model):
+  # 畳み込み層追加
+  model.add(Reshape((height, width, 1)))
+
+  # 畳み込み層1
+  model.add(Conv2D(32, (3, 3)))
   model.add(Activation("relu"))
+
+  # 畳み込み層2
+  model.add(Conv2D(32, (3, 3)))
+  model.add(Activation("relu"))
+
+  # ブーリング層
+  model.add(MaxPooling2D(2, 2))
+  model.add(Dropout(0.5))
+
+  # 畳み込み層3
+  model.add(Conv2D(16, (3, 3)))
+  model.add(Activation("relu"))
+  
+  # ブーリング層2
+  model.add(MaxPooling2D(2, 2))
+  model.add(Dropout(0.5))
+
+  model.add(Flatten())
+  # 全結合層
+  model.add(Dense(height * width))
+  model.add(Activation("relu"))
+  model.add(Dropout(0.5))
 
   # 出力層
   model.add(Dense(10))
   model.add(Activation("softmax"))
 
-  cv2.namedWindow('src')
-  cv2.namedWindow('dst')
+def learning(model):
+  # 学習実行
+  hist = model.fit(x_train, y_train, batch_size=200, epochs=10, verbose=1, validation_split=0.1)
   
-  model.compile(loss="catrgorical_crossentropy", optimizer="sgd", metrics=["accuracy"])
-
-  hist = model.fit(x_train, y_train, batch_size= 200, verbose=1, epochs=10, validation_split=0.1)
-
+  # 評価
   score = model.evaluate(x_test, y_test, verbose=1)
   print("Acc: ", score[1])
 
-tools = pyocr.get_available_tools()
-tool = tools[0]
-
-def camera():
+def camera(model):
   cap = cv2.VideoCapture(0)
   while True:
     ret, img_src = cap.read()
@@ -64,8 +107,10 @@ def camera():
 
     cv2.imshow('src', img)  # 入力画像表示
     # cv2.imshow('dst', img_dst)  # 出力画像表示
-    ch = cv2.waitKey(1)
-    if ch == ord('q'):
+    ch = cv2.waitKey(1) & 0xFF
+
+    prop_val = cv2.getWindowProperty('src', cv2.WND_PROP_ASPECT_RATIO)
+    if ch == ord('q') or (prop_val < 0) :
       break
     elif ch == ord('s'):
       flash(img, height_center, width_center)
@@ -73,6 +118,7 @@ def camera():
     
   cap.release()
   cv2.destroyAllWindows()
+  
 
 def ocr(img, width, height):
   img_dst = img[100:height-200, 100:width-200]
@@ -83,12 +129,29 @@ def ocr(img, width, height):
 
 def flash(frame, height, width):
   img = frame[height-70:height+70, width-70:width+70]
-  img =cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+  # グレースケール変換
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  
+  # 2値化処理
   _, th = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
   th = cv2.bitwise_not(th)
   th = cv2.GaussianBlur(th,(9,9), 0)
-  cv2.imwrite('image/capture/frame_img.png', img)
+  cv2.imwrite(cap_file, th)
 
-# camera()
-mnist()
+mnist(model)
+camera(model)
+
+Xt = []
+img = cv2.imread(cap_file, 0)
+img = cv2.resize(img, (int(width), int(height)), cv2.INTER_CUBIC)
+
+Xt.append(img)
+Xt = np.array(Xt) / 255
+
+# 学習済みモデルロード
+model = load_model(h5_file)
+
+# 判定
+predict = model.predict(Xt)
+result = np.argmax(predict, axis=1)
+print(result[0])
